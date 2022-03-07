f012829699a8a0445b479c67302d2d57edbbc9a3
GiovanniCalia <johnca@outlook.it>
2022-03-07 16:17:59 +0100

    Creazione titoli

--- js/script.js
+++ js/script.js
@@ -2,31 +2,38 @@ let sectionsImg = [
     {
         nome: "Wayne Barnett",
         job: "Founder $ CEO",
-        img: "img/wayne-barnett-founder-ceo.jpg"
+        img: "img/wayne-barnett-founder-ceo.jpg",
     }
 ];
-console.log(sectionsImg[0].nome)
+console.log(sectionsImg[0].nome);
 
 
 const eleContainer = document.querySelector(".team-container");
-const eleSection = document.getElementsByClassName("team-card");
+const eleSection = document.querySelector(".team-card");
 
+/*
 const img = document.createElement("img");
 img.src = "img/wayne-barnett-founder-ceo.jpg";
 img.innerHTML = "ciao";
 eleContainer.append(img);
+*/
 
 
-//cont.classList.add("ciao");
-//document.body.appendChild(img)
-//cont.style.width =`150px`;
-//cont.style.height =`150px`;
-//cont.innerHTML = "ciao";
-//console.log("cont",cont);
-
-
-
-
-
-
-
+const cont = document.createElement("div");
+cont.classList.add("team-card");
+const immagine = document.createElement("div");
+immagine.classList.add("card-image");
+const text = document.createElement("div");
+text.classList.add("card-text");
+const titleName = document.createElement("h3");
+const titleJob = document.createElement("p");
+text.append(titleName);
+text.append(titleJob);
+titleName.innerHTML = sectionsImg[0].nome;
+titleJob.innerHTML = sectionsImg[0].job;
+cont.append(immagine);
+cont.append(text);
+eleContainer.append(cont);
+console.log(cont);
+console.log(immagine);
+console.log(text);
